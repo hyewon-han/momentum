23fe90fdacafb0194021a1bbe6572e68b2625056
hyewon-han <hw1635@hanmail.net>
2023-05-25 17:29:40 +0900

    Create quotues.js

--- js/quotes.js
+++ js/quotes.js
@@ -50,3 +50,8 @@ const quotes = [
 
 const quote = document.querySelector("#quote span:first-child");
 const author = document.querySelector("#quote span:last-child");
+
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = `${todaysQuote.quote}`;
+author.innerText = `${todaysQuote.author}`;
