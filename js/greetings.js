611ff445aa9d55462da96275fdbceae914ab49a8
hyewon-han <hw1635@hanmail.net>
2023-05-28 21:21:36 +0900

    Create weather

--- js/greetings.js
+++ js/greetings.js
@@ -14,7 +14,7 @@ function onLoginSubmit(event) {
 }
 
 function paintGreetings(username) {
-  greeting.innerText = `Hello ${username}`;
+  greeting.innerText = `안녕 ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
---
+++ js/weather.js
@@ -0,0 +1,21 @@
+const API_KEY = "02102e3ccced740ad0260c330ad7fcd7";
+
+function onGeoOk(position) {
+  let lat = position.coords.latitude; //위도
+  let lon = position.coords.longitude; //경도
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((Response) => Response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+      city.innerText = data.name;
+    });
+}
+
+function onGeoError() {
+  alert("Can't find you. No weather for you");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
